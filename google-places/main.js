e0fa26e0af70a91536e38544fbb67183fede0e68
Luigi Rossi <l.rossi@outlook.com>
2016-05-13 20:16:15 +0100

    Update main.js

--- google-places/main.js
+++ google-places/main.js
@@ -1,88 +1,182 @@
-// see also: documentation for Google Places Javascript API
-// https://developers.google.com/maps/documentation/javascript/examples/place-search
-
-// global variables for convenience
-// call init() first to set these up
-// then they will be available in other functions
-var map;
-var service;
-var infowindow;
-
-function init(mapLocation) {
-  // set up map
-  map = new google.maps.Map(document.getElementById('map'), {
-      center: mapLocation,
-      zoom: 15
-  });
-
-  // set up popup window (to be shown when user clicks on a marker)
-  infowindow = new google.maps.InfoWindow();
-
-  // set up place API service
-  service = new google.maps.places.PlacesService(map);
-}
-
-function getPlaces(location){
-    // make API request
-    var request = {
-        location: location,
-        radius: '500',
-        types: ['cafe']
-    };
-    service.nearbySearch(request, placesCallback); // callback is called when API request is done
-}
-
-function placesCallback(results, status) {
-  // Uncomment to see results of API call:
-  // console.log(results);
-  // console.log(status);
-
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    // create a marker for each place returned
-    for (var i = 0; i < results.length; i++) {
-      var place = results[i];
-      createMarker(results[i]);
+var greyMap = [
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "hue": "#000000"
+            },
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": -100
+            },
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "all",
+        "stylers": [
+            {
+                "hue": "#000000"
+            },
+            {
+                "saturation": 0
+            },
+            {
+                "lightness": -100
+            },
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "hue": "#ffffff"
+            },
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 100
+            },
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "hue": "#000000"
+            },
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": -100
+            },
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "all",
+        "stylers": [
+            {
+                "hue": "#ffffff"
+            },
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 100
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "hue": "#ffffff"
+            },
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 100
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "hue": "#000000"
+            },
+            {
+                "saturation": 0
+            },
+            {
+                "lightness": -100
+            },
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "hue": "#000000"
+            },
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": -100
+            },
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "hue": "#bbbbbb"
+            },
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 26
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "hue": "#dddddd"
+            },
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": -3
+            },
+            {
+                "visibility": "on"
+            }
+        ]
     }
-  }
-}
-
-function createMarker(place) {
-  var placeLoc = place.geometry.location;
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location
-  });
-
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
-    infowindow.open(map, this);
-  });
-}
-
-function getDetails(placeId){
-    service.getDetails(
-      {
-        placeId: placeId
-      },
-        // here the callback function is written inline - this is an 'anonymous' function because
-        // it doesn't have a name. In other respects, it should work just the same as the function
-        // called placesCallback that we used in the getPlaces API request
-      function(place, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-            console.log(place);
-        }
-      }
-    );
-}
-
-
-// call init to kick things off!
-var center = new google.maps.LatLng(51.514756,-0.104345);
-init(center);
-
-// make places API request
-getPlaces(center);
-
-// test getting full place details - results should be console.logged out
-placeId = 'ChIJ4VeOyLIEdkgRJDgEopNEcSA';
-getDetails(placeId);
+];	
