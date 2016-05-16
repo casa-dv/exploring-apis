053c173444988b7e6dece9a0bf04f4386b425756
Tom Russell <tomalrussell@gmail.com>
2016-05-16 19:26:35 +0100

    rewrite jq filters to produce array of geojson features

--- eventbrite/jq-filter-expanded.sh
+++ eventbrite/jq-filter-expanded.sh
@@ -2,19 +2,29 @@
 cat events-expanded.json | jq '[
 	.[] |
 	{
-		start: .start.local,
-		name: .name.text,
-		"end": .["end"].local,
-    description: .description.text,
-		lat: .venue.address.latitude,
-		lon: .venue.address.longitude,
-		cat_id: .category.id,
-    cat_name: .category.short_name,
-    tickets: [.ticket_classes[] | {
-      free: .free,
-      name: .name,
-      description: .description,
-      cost: .cost.major_value
-    }]
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				.venue.address.longitude,
+				.venue.address.latitude
+			],
+		},
+		properties: {
+			start: .start.local,
+			name: .name.text,
+			"end": .["end"].local,
+			description: .description.text,
+			lat: .venue.address.latitude,
+			lon: .venue.address.longitude,
+			cat_id: .category.id,
+			cat_name: .category.short_name,
+			tickets: [.ticket_classes[] | {
+				free: .free,
+				name: .name,
+				description: .description,
+				cost: .cost.major_value
+			}]
+		}
 	}
 ]' > events-expanded-essential.json
--- eventbrite/jq-filter.sh
+++ eventbrite/jq-filter.sh
@@ -1,11 +1,19 @@
 cat events.json | jq '[
-	.[] | 
+	.[] |
 	{
-		start: .start.local, 
-		name: .name.text, 
-		"end": .["end"].local, 
-		lat: .venue.address.latitude, 
-		lon: .venue.address.longitude,
-		cat: .category_id
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+				.venue.address.longitude,
+				.venue.address.latitude
+			]
+		},
+		properties: {
+			start: .start.local,
+			name: .name.text,
+			"end": .["end"].local,
+			cat: .category_id
+		}
 	}
-]' > events-essential.json 
+]' > events-essential.json
